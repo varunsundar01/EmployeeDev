23a99d7ff090f827fb34ebebdc800e53d31ff984
Varun <varunsundar.0131@gmail.com>
2021-03-22 23:02:27 -0400

    Displayed process-details getters

--- src/pages/FinalReview.vue
+++ src/pages/FinalReview.vue
@@ -1,3 +1,53 @@
 <template>
-    <h1>Review</h1>
+  <base-card>
+    <div class="review-element">
+      <label>Problem Statement</label>
+      <p>{{ process.problem }}</p>
+    </div>
+    <div class="review-element">
+      <label>Proposed Solution</label>
+      <p>{{ process.solution }}</p>
+    </div>
+    <div class="review-element">
+      <label>Implementation Method</label>
+      <p>{{ process.implementation }}</p>
+    </div>
+  </base-card>
 </template>
+
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+
+    const process = computed(() => {
+      return store.getters["process/getProcess"];
+    });
+
+    return {
+      process,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.review-element {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2em;
+}
+
+label {
+    font-weight: 500;
+    font-size: 1.25em;
+}
+
+p {
+    font-weight: 300;
+    font-size: 1em;
+    margin: 0.25em 0 0 0;
+}
+</style>
