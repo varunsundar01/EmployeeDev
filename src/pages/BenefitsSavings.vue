bc5e6420d4bf604003309846028bdb3c14dd1b20
Varun <varunsundar.0131@gmail.com>
2021-03-22 23:01:49 -0400

    Removed form tag, added router to review page on submit button

--- src/pages/BenefitsSavings.vue
+++ src/pages/BenefitsSavings.vue
@@ -1,13 +1,31 @@
 <template>
   <base-card>
-    <form>
-      <form-element id="implementCost" inputStyle="currency">Implementation Cost</form-element>
-      <form-element id="savings" inputStyle="currency">Annual Cost Savings</form-element>
-      <div class="button-group">
-        <base-button primaryVisible="true">
-          <template v-slot:primary>Review</template>
-        </base-button>
-      </div>
-    </form>
+    <form-element id="implementCost" inputStyle="currency"
+      >Implementation Cost</form-element
+    >
+    <form-element id="savings" inputStyle="currency"
+      >Annual Cost Savings</form-element
+    >
+    <div class="button-group">
+      <base-button primaryVisible="true" @toNext="onSubmit">
+        <template v-slot:primary>Review</template>
+      </base-button>
+    </div>
   </base-card>
 </template>
+
+<script>
+import { useRouter } from "vue-router";
+export default {
+  setup() {
+    const router = useRouter();
+    function onSubmit() {
+      router.push("/final-review");
+    }
+
+    return {
+      onSubmit,
+    };
+  },
+};
+</script>
