769a4f9f3aaf96f4bfda9a99983a0c965ac0fc6c
Varun <varunsundar.0131@gmail.com>
2021-03-23 22:42:33 -0400

    Added transition and keep-alive to components

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div>
     <the-header></the-header>
     <div class="main-app">
-      <router-view></router-view>
+      <router-view v-slot="{ Component }">
+        <transition name="main" mode="out-in">
+          <keep-alive>
+            <component :is="Component" />
+          </keep-alive>
+        </transition>
+      </router-view>
     </div>
     <the-footer></the-footer>
   </div>
@@ -14,4 +20,21 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.main-enter-from,
+.main.leave-to {
+  opacity: 0.7;
+  transform: translateX(1%);
+}
+
+.main-enter-to,
+.main-leave-from {
+  opacity: 1;
+  transform: translateX(0%);
+}
+
+.main-enter-active,
+.main-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+</style>
