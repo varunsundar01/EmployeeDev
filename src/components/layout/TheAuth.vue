aefe7388a3d945672253dbf2a3c6fd4fcfbde858
Varun <varunsundar.0131@gmail.com>
2021-03-27 14:14:15 -0400

    Created separate component for sign in/sign up

---
+++ src/components/layout/TheAuth.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-card>
+    <form>
+      <form-element inputStyle="input" inputType="text">
+        Email
+      </form-element>
+      <form-element inputStyle="input" inputType="password">
+        Password
+      </form-element>
+    </form>
+    <base-button primaryVisible="true">
+      <template v-slot:primary>Sign In</template>
+    </base-button>
+    <p class="auth-option">Don't have an account? Sign Up instead</p>
+  </base-card>
+</template>
+
+<style scoped>
+.auth-option {
+  font-weight: 300;
+  text-align: center;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,26 +1,38 @@
 <template>
+  <div>
     <div class="hero-section">
-        <h1>EmployeeDev Portal</h1>
-        <p>Continuous Improvement and Learning propels are our core assets that drive growth</p>
-        <p>Sign in to submit your proposed process improvement methods and strategies</p>
-        <p>***PlaceHolder Image***</p>
-        <base-button primaryVisible="true">
-            <template v-slot:primary>Sign In</template>
-        </base-button>
+      <h1>EmployeeDev Portal</h1>
+      <p>
+        Sign in to submit your proposed process improvement methods and
+        strategies
+      </p>
+      <the-auth></the-auth>
     </div>
+    
+  </div>
 </template>
+
+<script>
+import TheAuth from '../components/layout/TheAuth.vue';
+export default {
+  components: {
+    TheAuth
+  }
+}
+</script>
 
 <style scoped>
 .hero-section {
-    margin-bottom: 4em;
+  margin-bottom: 2em;
 }
 .hero-section h1 {
-    font-size: 3em;
-    font-weight: 500;
+  font-size: 3em;
+  font-weight: 500;
+  margin: 0;
 }
 
 .hero-section p {
-    font-size: 1.25em;
-    font-weight: 300;
+  font-size: 1.25em;
+  font-weight: 300;
 }
 </style>
