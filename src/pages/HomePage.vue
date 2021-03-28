67505741220814b4e5cb0fcf22041a6dd6cbc424
Varun <varunsundar.0131@gmail.com>
2021-03-27 20:58:53 -0400

    Styled hero section heading

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="hero-section">
-      <h1>EmployeeDev Portal</h1>
+      <h1><span class="logo-first">Employee</span><span class="logo-second">Dev</span> Portal</h1>
       <p>
         Sign in to submit your proposed process improvement methods and
         strategies
@@ -29,10 +29,21 @@ export default {
   font-size: 3em;
   font-weight: 500;
   margin: 0;
+  color: var(--secondary);
 }
 
 .hero-section p {
   font-size: 1.25em;
   font-weight: 300;
+  color: var(--secondary);
+  margin: 0 auto 2em auto;
+}
+
+.logo-first {
+  font-weight: 300;
+}
+
+.logo-second {
+  color: var(--active);
 }
 </style>
