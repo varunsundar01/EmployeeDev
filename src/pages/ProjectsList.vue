aeac4abf43d7b8ecf26d5c8b5ea0072b25ebf31d
Varun <varunsundar.0131@gmail.com>
2021-03-30 23:42:33 -0400

    Added condition to check if projects data exists in localstorage, if not get from backend

--- src/pages/ProjectsList.vue
+++ src/pages/ProjectsList.vue
@@ -43,38 +43,43 @@ export default {
     let isLoading = ref(true);
     let filteredProject = ref([]);
 
-    fetch("http://127.0.0.1:8000/api/projects/")
-      .then((response) => response.json())
-      .then((data) => {
-        for (const value in data) {
-          const year = new Date(
-            Date.parse(data[value].created_at)
-          ).getFullYear();
-          const month = (
-            "0" + new Date(Date.parse(data[value].created_at)).getMonth()
-          ).slice(-2);
-          const day = (
-            "0" + new Date(Date.parse(data[value].created_at)).getDate()
-          ).slice(-2);
-          const createdDate = `${month}/${day}/${year}`;
+    if (localStorage.getItem("projects")) {
+      fetch("http://127.0.0.1:8000/api/projects/")
+        .then((response) => response.json())
+        .then((data) => {
+          for (const value in data) {
+            const year = new Date(
+              Date.parse(data[value].created_at)
+            ).getFullYear();
+            const month = (
+              "0" + new Date(Date.parse(data[value].created_at)).getMonth()
+            ).slice(-2);
+            const day = (
+              "0" + new Date(Date.parse(data[value].created_at)).getDate()
+            ).slice(-2);
+            const createdDate = `${month}/${day}/${year}`;
 
-          const ProjectObj = {
-            id: data[value].id,
-            projectName: data[value].project_name,
-            projectSlug: `/${data[value].project_slug}`,
-            problem: data[value].problem,
-            solution: data[value].solution,
-            implementation: data[value].implementation,
-            implementationCost: data[value].implementation_cost,
-            costSavings: data[value].cost_savings,
-            timeToComplete: data[value].time_to_complete,
-            createdAt: createdDate,
-          };
-          projects.value.push(ProjectObj);
-        }
-        localStorage.setItem("projects", JSON.stringify(projects));
-        isLoading.value = false;
-      });
+            const ProjectObj = {
+              id: data[value].id,
+              projectName: data[value].project_name,
+              projectSlug: `/${data[value].project_slug}`,
+              problem: data[value].problem,
+              solution: data[value].solution,
+              implementation: data[value].implementation,
+              implementationCost: data[value].implementation_cost,
+              costSavings: data[value].cost_savings,
+              timeToComplete: data[value].time_to_complete,
+              createdAt: createdDate,
+            };
+            projects.value.push(ProjectObj);
+          }
+          localStorage.setItem("projects", JSON.stringify(projects));
+          isLoading.value = false;
+        });
+    } else {
+      projects.value = JSON.parse(localStorage.getItem("projects"))._value;
+      isLoading.value = false;
+    }
 
     function filterResults(data) {
       filteredProject.value = projects.value.filter((project) => {
