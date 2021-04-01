5a42d50b8c5a389c7e72d1db5e5dc2c35a38ab1d
Varun <varunsundar.0131@gmail.com>
2021-04-01 18:20:32 -0400

    Added Dashboard page and titles to every route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from './pages/HomePage.vue';
+import TheDashboard from './pages/auth/TheDashboard.vue';
 import ProcessDetails from './pages/form/ProcessDetails.vue';
 import BenefitsSavings from './pages/form/BenefitsSavings.vue';
 import FinalReview from './pages/form/FinalReview.vue';
@@ -12,13 +13,19 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage },
-        { name: 'process-details', path: '/process-details', component: ProcessDetails },
-        { name: 'benefits-savings', path: '/benefits-savings', component: BenefitsSavings },
-        { name: 'final-review', path: '/final-review', component: FinalReview },
-        { name: 'sign-up', path: '/sign-up', component: SignUp },
-        { name: 'projects-list', path: '/projects', component: ProjectsList },
+        { name: 'the-dashboard', path: '/dashboard', component: TheDashboard, meta: { title: 'Dashboard' } },
+        { name: 'process-details', path: '/process-details', component: ProcessDetails, meta: { title: 'Process Details' } },
+        { name: 'benefits-savings', path: '/benefits-savings', component: BenefitsSavings, meta: { title: 'Benefitss & Savings' } },
+        { name: 'final-review', path: '/final-review', component: FinalReview, meta: { title: 'Final Review' } },
+        { name: 'sign-up', path: '/sign-up', component: SignUp, meta: { title: 'Sign Up' } },
+        { name: 'projects-list', path: '/projects', component: ProjectsList, meta: { title: 'Projects' } },
         { name: 'projects-detail', path: '/projects/:slug', component: ProjectsDetail },
     ]
+});
+
+router.beforeEach((to, _, next) => {
+    document.title = to.meta.title || 'EmployeeDev';
+    next();
 });
 
 export default router;
