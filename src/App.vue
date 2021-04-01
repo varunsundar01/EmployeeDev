9808c64eed45bcd48bb85be3af6f7823d242c445
Varun <varunsundar.0131@gmail.com>
2021-04-01 18:22:22 -0400

    Removed page transitions

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
   <div>
     <the-header></the-header>
     <div class="main-app">
-      <the-tab v-if="currentRoute === '/process-details' || currentRoute === '/benefits-savings' || currentRoute === '/final-review'"></the-tab>
+      <the-tab
+        v-if="
+          currentRoute === '/process-details' ||
+            currentRoute === '/benefits-savings' ||
+            currentRoute === '/final-review'
+        "
+      ></the-tab>
       <router-view v-slot="{ Component }">
-        <transition name="main" mode="out-in">
-          <keep-alive>
-            <component :is="Component" />
-          </keep-alive>
-        </transition>
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
       </router-view>
     </div>
     <the-footer></the-footer>
@@ -35,22 +39,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.main-enter-from,
-.main.leave-to {
-  opacity: 0.7;
-  transform: translateX(1%);
-}
-
-.main-enter-to,
-.main-leave-from {
-  opacity: 1;
-  transform: translateX(0%);
-}
-
-.main-enter-active,
-.main-leave-active {
-  transition: all 0.5s ease-in-out;
-}
-</style>
