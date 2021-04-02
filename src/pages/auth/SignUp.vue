da58da3dd22bdba7f9545527becb92a5fbfc79fd
Varun <varunsundar.0131@gmail.com>
2021-04-02 00:28:35 -0400

    Created sign up form

--- src/pages/auth/SignUp.vue
+++ src/pages/auth/SignUp.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>Sign Up</h1>
+  <base-card>
+    <h1 class="title">Create your Account</h1>
+    <form-element
+      inputStyle="input"
+      inputType="text"
+      @enteredInput="enteredInput"
+      id="firstName"
+      >First Name</form-element
+    >
+    <form-element
+      inputStyle="input"
+      inputType="text"
+      @enteredInput="enteredInput"
+      id="lastName"
+      >Last Name</form-element
+    >
+    <form-element
+      inputStyle="input"
+      inputType="email"
+      @enteredInput="enteredInput"
+      id="email"
+      >Email</form-element
+    >
+    <form-element
+      inputStyle="input"
+      inputType="password"
+      @enteredInput="enteredInput"
+      id="password"
+      >Password</form-element
+    >
+    <form-element
+      inputStyle="input"
+      inputType="password"
+      @enteredInput="enteredInput"
+      id="password"
+      >Confirm Password</form-element
+    >
+
+    <base-button @toNext="onSubmit" primaryVisible="true">
+        <template v-slot:primary>Create Account</template>
+    </base-button>
+  </base-card>
 </template>
+
+<style scoped>
+.title {
+    font-weight: 500;
+    margin: 0.5em auto 1em auto;
+}
+</style>
