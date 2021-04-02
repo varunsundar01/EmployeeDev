1e570113559e8a70960b5659f98a967938323f39
Varun <varunsundar.0131@gmail.com>
2021-04-02 16:44:58 -0400

    Added conditions to "keep alive" page and added computed function to add nav tab

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,12 @@
   <div>
     <the-header></the-header>
     <div class="main-app">
-      <the-tab
-        v-if="
-          currentRoute === '/process-details' ||
-            currentRoute === '/benefits-savings' ||
-            currentRoute === '/final-review'
-        "
-      ></the-tab>
+      <the-tab v-if="tabCheck"></the-tab>
       <router-view v-slot="{ Component }">
-        <keep-alive>
+        <keep-alive v-if="!$store.getters.checkSubmit.submitted && $route.meta.keepAlive">
           <component :is="Component" />
         </keep-alive>
+        <component v-else :is="Component" />
       </router-view>
     </div>
     <the-footer></the-footer>
@@ -30,11 +25,17 @@ export default {
   name: "App",
   setup() {
     const route = useRoute();
-    let currentRoute = computed(() => {
-      return route.path;
+
+    let tabCheck = computed(() => {
+      return (
+        route.path === "/process-details" ||
+        route.path === "/benefits-savings" ||
+        route.path === "/final-review"
+      );
     });
+
     return {
-      currentRoute,
+      tabCheck,
     };
   },
 };
