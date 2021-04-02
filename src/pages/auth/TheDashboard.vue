673df2abff4d8f8a6694d01e63d4df3c575d7aa7
Varun <varunsundar.0131@gmail.com>
2021-04-02 16:50:09 -0400

    Added title styling and banner to show form submission success

--- src/pages/auth/TheDashboard.vue
+++ src/pages/auth/TheDashboard.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>Dashboard</h1>
+  <div>
+    <h1 class="title">Dashboard</h1>
+    <the-banner v-if="displayBanner">{{
+      $store.getters.checkSubmit.submitMessage
+    }}</the-banner>
+
+    <p>Projects list of current user</p>
+  </div>
 </template>
+
+<script>
+import TheBanner from "../../components/UI/TheBanner.vue";
+import { ref, computed } from "vue";
+import { useStore } from "vuex";
+export default {
+  components: {
+    TheBanner,
+  },
+
+  setup() {
+    const store = useStore();
+    const submitMessage = ref("");
+
+    const displayBanner = computed(() => {
+      if (store.getters.checkSubmit.submitted) {
+        return true;
+      }
+      return false;
+    });
+
+    return {
+      displayBanner,
+      submitMessage,
+    };
+  },
+};
+</script>
