93c7559c13929c40a3e3183ad2b0ac6c7b441589
Varun <varunsundar.0131@gmail.com>
2021-04-02 00:29:46 -0400

    Created user registration and login functionality using tokens

--- employee_dev/accounts/serializers.py
+++ employee_dev/accounts/serializers.py
@@ -5,16 +5,21 @@ from django.contrib.auth import authenticate
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'first_name', 'last_name', 'username', 'email')
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'first_name', 'last_name', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(
+            validated_data['username'], 
+            validated_data['email'], 
+            validated_data['password'],
+            first_name = validated_data['first_name'], 
+            last_name = validated_data['last_name'])
         return user
 
 class LoginSerializer(serializers.Serializer):
