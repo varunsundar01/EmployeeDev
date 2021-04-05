f9c2bb7f9a642b6555b3554add270e9d21dfadd3
Varun <varunsundar.0131@gmail.com>
2021-04-04 21:31:25 -0400

    Added variables to reset data based on routes

--- src/router.js
+++ src/router.js
@@ -8,23 +8,35 @@ import FinalReview from './pages/form/FinalReview.vue';
 import SignUp from './pages/auth/SignUp.vue';
 import ProjectsList from './pages/projects/ProjectsList.vue';
 import ProjectsDetail from './pages/projects/ProjectsDetail.vue';
+import store from './store';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage },
         { name: 'the-dashboard', path: '/dashboard', component: TheDashboard, meta: { title: 'Dashboard' } },
-        { name: 'process-details', path: '/process-details', component: ProcessDetails, meta: { title: 'Process Details', keepAlive: true } },
-        { name: 'benefits-savings', path: '/benefits-savings', component: BenefitsSavings, meta: { title: 'Benefitss & Savings', keepAlive: true } },
-        { name: 'final-review', path: '/final-review', component: FinalReview, meta: { title: 'Final Review', keepAlive: true } },
+        { name: 'process-details', path: '/process-details', component: ProcessDetails, meta: { title: 'Process Details', keepAlive: true, form: true } },
+        { name: 'benefits-savings', path: '/benefits-savings', component: BenefitsSavings, meta: { title: 'Benefits & Savings', keepAlive: true, form: true } },
+        { name: 'final-review', path: '/final-review', component: FinalReview, meta: { title: 'Final Review', keepAlive: true, form: true } },
         { name: 'sign-up', path: '/sign-up', component: SignUp, meta: { title: 'Sign Up' } },
         { name: 'projects-list', path: '/projects', component: ProjectsList, meta: { title: 'Projects' } },
         { name: 'projects-detail', path: '/projects/:slug', component: ProjectsDetail },
     ]
 });
 
-router.beforeEach((to, _, next) => {
+router.beforeEach((to, from, next) => {
     document.title = to.meta.title || 'EmployeeDev';
+
+    if (from.meta.form && !to.meta.form) {
+        store.dispatch("initializeValues");
+    }
+    if (from.fullPath === "/dashboard" && store.getters.checkSubmit.submitted) {
+        store.dispatch("switchSubmit", {
+            message: "",
+            messageType: 201,
+            value: false
+        })
+    }
     next();
 });
 
