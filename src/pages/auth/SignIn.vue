2f268c6c4b556271e6c696d616b019ce0bdcc1df
Varun <varunsundar.0131@gmail.com>
2021-04-10 11:50:15 -0400

    Added SignIn page

---
+++ src/pages/auth/SignIn.vue
@@ -0,0 +1,21 @@
+<template>
+    <base-card>
+        <h1 class="title">Sign In</h1>
+        <the-auth></the-auth>
+    </base-card>
+</template>
+
+<script>
+import TheAuth from "../../components/auth/TheAuth.vue";
+export default {
+    components: {
+        TheAuth
+    }
+}
+</script>
+
+<style scoped>
+.title {
+    margin: 0.5em auto;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import ProcessDetails from './pages/form/ProcessDetails.vue';
 import BenefitsSavings from './pages/form/BenefitsSavings.vue';
 import FinalReview from './pages/form/FinalReview.vue';
 import SignUp from './pages/auth/SignUp.vue';
+import SignIn from './pages/auth/SignIn.vue';
 import ProjectsList from './pages/projects/ProjectsList.vue';
 import ProjectsDetail from './pages/projects/ProjectsDetail.vue';
 import store from './store';
@@ -19,6 +20,7 @@ const router = createRouter({
         { name: 'benefits-savings', path: '/benefits-savings', component: BenefitsSavings, meta: { title: 'Benefits & Savings', keepAlive: true, form: true } },
         { name: 'final-review', path: '/final-review', component: FinalReview, meta: { title: 'Final Review', keepAlive: true, form: true } },
         { name: 'sign-up', path: '/sign-up', component: SignUp, meta: { title: 'Sign Up' } },
+        { name: 'sign-in', path: '/sign-in', component: SignIn, meta: { title: 'Sign In' } },
         { name: 'projects-list', path: '/projects', component: ProjectsList, meta: { title: 'Projects' } },
         { name: 'projects-detail', path: '/projects/:slug', component: ProjectsDetail },
     ]
