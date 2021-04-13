03f747d23f2c4e30f7e3fd08adb4e347f554200e
Varun <varunsundar.0131@gmail.com>
2021-04-13 15:24:38 -0400

    Removed banner from signup component and moved to auth component

--- src/components/auth/TheAuth.vue
+++ src/components/auth/TheAuth.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <the-banner v-if="showBanner">{{ bannerMessage }}</the-banner>
     <form>
       <form-element
         inputStyle="input"
@@ -29,28 +30,55 @@
 </template>
 
 <script>
-import {reactive} from "vue";
+import { reactive, computed } from "vue";
 import { useStore } from "vuex";
+import TheBanner from "../UI/TheBanner.vue";
 export default {
+  components: {
+    TheBanner,
+  },
   setup() {
     const store = useStore();
 
     const user = reactive({
       email: "",
-      password: ""
-    })
+      password: "",
+    });
 
     function enteredInput(data) {
       user[data.id] = data.value;
     }
 
     function signIn() {
+      store.dispatch("auth/resetMessages");
       store.dispatch("auth/signIn", user);
     }
 
+    let showBanner = computed(() => {
+      if (
+        store.getters["auth/getSubmitMessage"] !== "" ||
+        store.getters["auth/getError"].authError
+      ) {
+        return true;
+      }
+      return false;
+    });
+
+    let bannerMessage = computed(() => {
+      if (store.getters["auth/getSubmitMessage"] !== "") {
+        return store.getters["auth/getSubmitMessage"];
+      } else if (store.getters["auth/getError"].authError) {
+        return store.getters["auth/getError"].errorMessage;
+      } else {
+        return "";
+      }
+    });
+
     return {
       enteredInput,
-      signIn
+      signIn,
+      showBanner,
+      bannerMessage,
     };
   },
 };
--- src/pages/auth/SignIn.vue
+++ src/pages/auth/SignIn.vue
@@ -1,6 +1,5 @@
 <template>
   <base-card>
-    <the-banner v-if="showBanner">{{ bannerMessage }}</the-banner>
     <h1 class="title">Sign In</h1>
     <the-auth></the-auth>
   </base-card>
@@ -8,13 +7,11 @@
 
 <script>
 import TheAuth from "../../components/auth/TheAuth.vue";
-import TheBanner from "../../components/UI/TheBanner.vue";
 import { useStore } from "vuex";
-import { ref, computed } from "vue";
+import { ref } from "vue";
 export default {
   components: {
-    TheAuth,
-    TheBanner,
+    TheAuth
   },
   setup() {
     window.scrollTo(0, 0);
@@ -23,27 +20,8 @@ export default {
 
     const submitMessage = ref(store.getters["auth/getSubmitMessage"]);
 
-    let showBanner = computed(() => {
-      if (store.getters['auth/getSubmitMessage'] !== '' || store.getters['auth/getError'].authError) {
-        return true;
-      }
-      return false;
-    })
-
-    let bannerMessage = computed(() => {
-      if (store.getters['auth/getSubmitMessage'] !== '') {
-        return store.getters['auth/getSubmitMessage'];
-      } else if (store.getters['auth/getError'].authError) {
-        return store.getters['auth/getError'].errorMessage;
-      } else {
-        return "";
-      }
-    })
-
     return {
-        submitMessage,
-        showBanner,
-        bannerMessage
+        submitMessage
     }
   },
 };
