804517b3122b27bf221b22b43e910b6a19249b8e
Varun <varunsundar.0131@gmail.com>
2021-04-13 21:08:32 -0400

    Combined all error conditions together and added list-element to display user projects

--- src/pages/auth/TheDashboard.vue
+++ src/pages/auth/TheDashboard.vue
@@ -2,63 +2,90 @@
   <div>
     <h1 class="title">Dashboard</h1>
     <the-banner v-if="displayBanner">{{ bannerMessage }}</the-banner>
-    <the-banner v-if="errorBanner">{{ errorMessage }}</the-banner>
-    
-    <p>Projects list of current user</p>
+
+    <p class="dashboard-head">Hey, {{firstName}}. You can find all the projects you have submitted here.</p>
+    <p class="dashboard-head">Click <router-link to="/projects">here</router-link> to view projects submitted by your colleagues</p>
+    <list-element
+      v-for="project in $store.getters['projects/getUserProjects']"
+      :key="project.id"
+      :id="project.id"
+      :title="project.project_name"
+      author="Placeholder Author"
+      :createdAt="project.createdAt"
+      :slug="project.project_slug"
+      @deleteProject="deleteProjectDialog"
+    ></list-element>
   </div>
 </template>
 
 <script>
 import TheBanner from "../../components/UI/TheBanner.vue";
+import ListElement from "../../components/UI/ListElement.vue";
 import { ref, computed } from "vue";
 import { useStore } from "vuex";
 export default {
   components: {
     TheBanner,
+    ListElement,
   },
 
   setup() {
     const store = useStore();
     const submitMessage = ref("");
+    const firstName = ref(localStorage.getItem("firstName"));
+
+    store.dispatch("projects/loadUserProjects");
 
     const displayBanner = computed(() => {
-      if (store.getters['projects/checkSubmit'].submitted || store.getters['auth/getSubmitMessage'] !== "") {
+      if (
+        store.getters["projects/checkSubmit"].submitted ||
+        store.getters["auth/getSubmitMessage"] !== ""
+      ) {
         return true;
       }
       return false;
     });
 
     const bannerMessage = computed(() => {
-      if (store.getters['projects/checkSubmit'].submitMessage !== "") {
-        return store.getters['projects/checkSubmit'].submitMessage;
-      } else if (store.getters['auth/getSubmitMessage'] !== "") {
-        return store.getters['auth/getSubmitMessage'];
+      if (store.getters["projects/checkSubmit"].submitMessage !== "") {
+        return store.getters["projects/checkSubmit"].submitMessage;
+      } else if (store.getters["auth/getSubmitMessage"] !== "") {
+        return store.getters["auth/getSubmitMessage"];
+      } else if (store.getters["projects/checkError"].errorActive) {
+        return store.getters["projects/checkError"].errorMessage;
       } else {
         return "";
       }
-    })
-
-    const errorBanner = computed(() => {
-      if (store.getters['auth/getError'].authError) {
-        return true;
-      }
-      return false;
     });
 
-    const errorMessage = computed(() => {
-      if (store.getters['auth/getError'].authError) {
-        return store.getters['auth/getError'].errorMessage;
-      } 
-      return "";
-    })
+    // const errorBanner = computed(() => {
+    //   if (store.getters['auth/getError'].authError || store.getters['projects/checkError']) {
+    //     return true;
+    //   }
+    //   return false;
+    // });
+
+    // const errorMessage = computed(() => {
+    //   if (store.getters['auth/getError'].authError) {
+    //     return store.getters['auth/getError'].errorMessage;
+    //   }
+    //   return "";
+    // })
 
     return {
       displayBanner,
       submitMessage,
       bannerMessage,
-      errorBanner,
-      errorMessage
+      firstName
+      // errorBanner,
+      // errorMessage
     };
   },
 };
 </script>
+
+<style scoped>
+.dashboard-head {
+  margin-bottom: 1em;
+}
+</style>
