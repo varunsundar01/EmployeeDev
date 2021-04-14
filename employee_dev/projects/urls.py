45a0d3957ed24931305a156e91c6978779a6a217
Varun <varunsundar.0131@gmail.com>
2021-04-13 21:06:26 -0400

    Added url for dashboard view

--- employee_dev/projects/urls.py
+++ employee_dev/projects/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
-from projects.api import ProjectViewset
+from projects.api import ProjectViewset, ProjectDashboardViewset
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewset)
+router.register('userprojects', ProjectDashboardViewset, basename='userproject')
 
 urlpatterns = router.urls
