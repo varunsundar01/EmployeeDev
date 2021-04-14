d5d2b835c238faf1a78b4be2c51122587d81abbd
Varun <varunsundar.0131@gmail.com>
2021-04-13 21:05:30 -0400

    Added view for user projects to be shown in the dashboard

--- employee_dev/projects/api.py
+++ employee_dev/projects/api.py
@@ -1,6 +1,9 @@
 from projects.models import Project
 from rest_framework import viewsets, permissions
 from .serializers import ProjectSerializer
+from django.contrib.auth import get_user_model
+
+Employee = get_user_model()
 
 class ProjectViewset(viewsets.ModelViewSet):
     queryset = Project.objects.all()
@@ -8,3 +11,15 @@ class ProjectViewset(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = ProjectSerializer
+
+class ProjectDashboardViewset(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return self.request.user.projects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(employee=self.request.user)
