a5d1e3f4d51e29c9027456df764c0ca31bce532d
Varun <varunsundar.0131@gmail.com>
2021-04-15 23:28:44 -0400

    Added icons to footer

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="footer">
     <p>&copy; {{ currentYear }} | Built by <a href="#">Varun</a></p>
-    <p>Github | Twitter | LinkedIn | Email</p>
+    <div class="social-icons">
+      <i class="icon-github-circled"></i>
+      <i class="icon-twitter"></i>
+      <i class="icon-linkedin"></i>
+      <i class="icon-mail-alt"></i>
+    </div>
   </div>
 </template>
 
@@ -29,5 +34,22 @@ export default {
 p {
   color: rgba(0, 0, 0, 0.5);
   font-size: 1.1em;
+}
+
+.social-icons {
+  margin: 0 auto 2em auto;
+}
+
+.social-icons i {
+  color: var(--secondary);
+  opacity: 0.5;
+  margin: 0 1em;
+  font-size: 1.75em;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+.social-icons i:hover {
+  opacity: 1;
 }
 </style>
