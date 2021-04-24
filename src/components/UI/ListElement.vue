9387fece1e34032ed1e9658f6cee110526ff1141
Varun <varunsundar.0131@gmail.com>
2021-04-24 14:20:31 -0400

    Added "Delete" text next to trash icon and added styling

--- src/components/UI/ListElement.vue
+++ src/components/UI/ListElement.vue
@@ -10,7 +10,7 @@
       class="icon-trash"
       @click="deleteProject"
       v-if="displayProjectOptions"
-    ></i>
+    ><small>Delete</small></i>
 
     <router-link :to="'/projects/' + slug"
       ><button class="project-button view">View</button></router-link
@@ -54,7 +54,7 @@ export default {
 <style scoped>
 .list-element {
   background-color: var(--background-light);
-  padding: 0.55em 1.25em;
+  padding: 1.85em 1.25em;
   border-radius: 5px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.05);
   margin-bottom: 1em;
@@ -63,17 +63,27 @@ export default {
 
 i.icon-trash {
   position: absolute;
-  right: 15px;
-  top: 15px;
+  right: 5px;
+  top: 5px;
   cursor: pointer;
   transition: all 0.3s ease-in-out;
   color: var(--secondary);
   opacity: 0.6;
+  border: 1px solid transparent;
+  padding: 0.25em 0.5em;
+  border-radius: 5px;
 }
 
 i.icon-trash:hover {
+  background-color: var(--error-background);
+  border: 1px solid var(--error-border);
   color: var(--error-button);
   opacity: 1;
+}
+
+i small {
+  font-style: normal;
+  margin-right: 0.2em;
 }
 
 .project-title {
