36428ff7cc5f75bf1ae68ce881cffec64faaf699
Varun <varunsundar.0131@gmail.com>
2021-04-23 23:47:41 -0400

    Verify User logic added

--- src/pages/auth/VerifyAccount.vue
+++ src/pages/auth/VerifyAccount.vue
@@ -1,5 +1,31 @@
 <template>
   <base-card>
     <h1 class="title">Verify Your Account</h1>
+    <the-banner></the-banner>
   </base-card>
 </template>
+
+<script>
+import { useRoute, useRouter } from "vue-router";
+import { useStore } from "vuex";
+import TheBanner from "../../components/UI/TheBanner.vue";
+export default {
+  components: {
+    TheBanner
+  },
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+    const store = useStore();
+
+    if (!route.query.req_u && !route.query.reset) {
+      router.push("/sign-in");
+    }
+
+    store.dispatch('auth/verifyUser', {
+      uidb64: route.query.req_u,
+      token: route.query.reset
+    });
+  }
+};
+</script>
