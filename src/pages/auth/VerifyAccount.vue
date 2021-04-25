2e56f9ae59c09ca79fc018e2cee80b0feeaa617e
Varun <varunsundar.0131@gmail.com>
2021-04-25 01:12:22 -0400

    Added error message/submit message

--- src/pages/auth/VerifyAccount.vue
+++ src/pages/auth/VerifyAccount.vue
@@ -1,17 +1,18 @@
 <template>
   <base-card>
     <h1 class="title">Verify Your Account</h1>
-    <the-banner></the-banner>
+    <the-banner>{{ bannerMessage }}</the-banner>
   </base-card>
 </template>
 
 <script>
 import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
+import { computed } from "vue";
 import TheBanner from "../../components/UI/TheBanner.vue";
 export default {
   components: {
-    TheBanner
+    TheBanner,
   },
   setup() {
     const route = useRoute();
@@ -22,10 +23,24 @@ export default {
       router.push("/sign-in");
     }
 
-    store.dispatch('auth/verifyUser', {
-      uidb64: route.query.req_u,
-      token: route.query.reset
+    const bannerMessage = computed(() => {
+      if (store.getters['auth/getError'].authError) {
+        return store.getters['auth/getError'].errorMessage;
+      } else if (store.getters['auth/getSubmitMessage'] !== "") {
+        return store.getters['auth/getSubmitMessage'];
+      } else {
+        return "";
+      }
     });
-  }
+
+    store.dispatch("auth/verifyUser", {
+      uidb64: route.query.req_u,
+      token: route.query.reset,
+    });
+
+    return {
+      bannerMessage,
+    };
+  },
 };
 </script>
